142bce4f1f5a7150aa8686f1d2acd1a019a7b41e
jakobeha <jakobeha@fastmail.com>
2022-04-26 02:48:28 -0400

    added 2 functions

--- src/record.ts
+++ src/record.ts
@@ -10,6 +10,25 @@ export module Record {
     }
     return result as Record<K, VO>
   }
+  /**
+   * If TypeScript is complaining about `Partial` *don't* use this function, use `PartialRecord` instead.
+   * With this method, you can actually get an unchecked type error if one of the fields is `undefined` and your transformer isn't equipped to handle it.
+   */
+  export function mapNotNullValues<K extends string | number | symbol, VI, VO> (record: Record<K, VI>, transform: (value: VI, key: K) => VO | null | undefined): Partial<Record<K, VO>> {
+    const result: Partial<Record<K, VO>> = {}
+    for (const key in record) {
+      const newValue = transform(record[key], key)
+      if (newValue !== null && newValue !== undefined) {
+        result[key] = newValue
+      }
+    }
+    return result
+  }
+
+  /** Same as `mapNotNullValues` but looser types, needed sometimes for typechecking */
+  export function mapNotNullValues2<VI, VO> (record: { [key: string]: VI }, transform: (value: VI, key: string) => VO | null | undefined): { [key: string]: VO } {
+    return mapNotNullValues(record, transform) as { [key: string]: VO }
+  }
 
   export async function forEachConcurrent<K extends string, V> (record: Record<K, V>, action: (key: K, value: V) => Promise<void>): Promise<void> {
     // eslint-disable-next-line @typescript-eslint/promise-function-async
@@ -29,10 +48,25 @@ export module PartialRecord {
   export function mapValues<K extends string | number | symbol, VI, VO> (record: Partial<Record<K, VI>>, transform: (value: VI, key: K) => VO): Partial<Record<K, VO>> {
     const result: Partial<Record<K, VO>> = {}
     for (const key in record) {
-      if (record[key] !== undefined) {
-        result[key] = transform(record[key]!, key)
+      const oldValue = record[key]
+      if (oldValue !== undefined) {
+        result[key] = transform(oldValue, key)
       }
     }
     return result as Record<K, VO>
+  }
+
+  export function mapNotNullValues<K extends string | number | symbol, VI, VO> (record: Partial<Record<K, VI>>, transform: (value: VI, key: K) => VO | null | undefined): Partial<Record<K, VO>> {
+    const result: Partial<Record<K, VO>> = {}
+    for (const key in record) {
+      const oldValue = record[key]
+      if (oldValue !== undefined) {
+        const newValue = transform(oldValue, key)
+        if (newValue !== null && newValue !== undefined) {
+          result[key] = newValue
+        }
+      }
+    }
+    return result
   }
 }
